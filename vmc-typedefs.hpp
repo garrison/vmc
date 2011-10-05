090badda777dc88bd2615608911de979121cf8ef
Jim Garrison <jim@garrison.cc>
2011-10-05 14:51:17 -0700

    basic accumulator class; we'll make more of these later

--- vmc-typedefs.hpp
+++ vmc-typedefs.hpp
@@ -6,4 +6,32 @@
 typedef double probability_t;
 typedef std::complex<double> amplitude_t;
 
+class accumulator_t
+{
+public:
+    accumulator_t (double x=0)
+	: accum(x)
+	{
+	}
+
+    operator double ()
+	{
+	    return accum;
+	}
+
+    accumulator_t & operator+= (double x)
+	{
+	    accum += x;
+	    return *this;
+	}
+
+    accumulator_t & operator-= (double x)
+	{
+	    return operator+=(-x);
+	}
+
+private:
+    long double accum;
+};
+
 #endif

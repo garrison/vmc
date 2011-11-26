48819c743748cc19b8f7a04af4ba4a681f752b32
Jim Garrison <garrison@physics.ucsb.edu>
2011-11-25 20:15:47 -0800

    ability to specify random seed through json

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 #include <vector>
 #include <list>
 #include <memory>
+#include <exception>
+#include <cstring>
 
+#include <json/json.h>
+#include <boost/array.hpp>
+#include <boost/assert.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
 
@@ -23,14 +28,99 @@
 #include "lowest-momenta.hpp"
 #include "array-util.hpp"
 
-const unsigned int seed = 13;
+class ParseError : public std::exception
+{
+public:
+    ParseError (void)
+        : error_message(default_error_message)
+        {
+        }
 
-const unsigned int DIMENSION = 1;
-const unsigned int lattice_length = 40;
-const unsigned int F = 20;
+    ParseError (const char *error_message_)
+        : error_message(error_message_)
+        {
+        }
+
+    virtual const char * what (void) const throw()
+        {
+            return error_message;
+        }
+
+private:
+    static const char *default_error_message;
+    const char *error_message;
+};
+
+const char *ParseError::default_error_message = "json input error";
+
+static inline void ensure_object (const Json::Value &jsonvalue)
+{
+    if (!jsonvalue.isObject())
+        throw ParseError("object expected");
+}
+
+static void ensure_required (const Json::Value &jsonvalue, const char * const keys[])
+{
+    BOOST_ASSERT(jsonvalue.isObject());
+    for (const char * const *key = keys; *key != NULL; ++key) {
+        if (!jsonvalue.isMember(*key))
+            throw ParseError("required keys not all given");
+    }
+}
+
+static void ensure_only (const Json::Value &jsonvalue, const char * const keys[])
+{
+    BOOST_ASSERT(jsonvalue.isObject());
+    for (Json::Value::const_iterator i = jsonvalue.begin(), e = jsonvalue.end(); i != e; ++i) {
+        const char * const *key = keys;
+        for (; *key != NULL; ++key) {
+            if (strcmp(i.memberName(), *key) == 0)
+                break;
+        }
+        if (key == NULL)
+            throw ParseError("too many keys provided");
+    }
+}
 
 int main ()
 {
+    unsigned int seed = 0;
+
+    // take json input and perform a simulation
+
+    Json::Value json_input;
+    std::cin >> json_input;
+
+    try {
+        ensure_object(json_input);
+        const char * const json_input_required[] = { "rng", NULL };
+        ensure_required(json_input, json_input_required);
+        ensure_only(json_input, json_input_required);
+
+        // initialize random number generator
+        const Json::Value &json_rng = json_input["rng"];
+        ensure_object(json_rng);
+        const char * const json_rng_allowed[] = { "seed", NULL };
+        ensure_only(json_rng, json_rng_allowed);
+        if (json_rng.isMember("seed")) {
+            if (!json_rng["seed"].isIntegral())
+                throw ParseError("seed must be correct data type");
+            seed = json_rng["seed"].asUInt();
+        } else {
+            throw ParseError("seed must be given");
+        }
+
+    } catch (ParseError e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    // example API usage
+
+    const unsigned int DIMENSION = 1;
+    const unsigned int lattice_length = 40;
+    const unsigned int F = 20;
+
     rng_class rng(seed);
 
     boost::shared_ptr<const HypercubicLattice<DIMENSION> > lattice(new HypercubicLattice<DIMENSION>(make_array<int, DIMENSION>(lattice_length)));

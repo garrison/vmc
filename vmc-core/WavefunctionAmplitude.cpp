148d36588e84fc624818d15da4bc7ba5d82a22e4
Jim Garrison <garrison@physics.ucsb.edu>
2012-04-12 16:00:51 -0700

    removed unnecessary line

--- vmc-core/WavefunctionAmplitude.cpp
+++ vmc-core/WavefunctionAmplitude.cpp
@@ -4,8 +4,6 @@
 
 void WavefunctionAmplitude::reset_with_filler (const RandomFiller &filler, rng_class &rng)
 {
-    const PositionArguments &r = get_positions();
-
     std::vector<std::vector<unsigned int> > vv;
     for (unsigned int i = 0; i < r.get_N_species(); ++i)
         vv.push_back(filler.some_random_filling(r.get_N_filled(i), rng));

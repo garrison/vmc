8ee11c2835853436950038d6c8339b883ca25de3
Jim Garrison <garrison@physics.ucsb.edu>
2012-05-08 14:10:54 -0700

    enforce_boundary() can now accept either a BravaisSite or a LatticeSite

--- pyvmc/core/boundary_conditions.py
+++ pyvmc/core/boundary_conditions.py
@@ -2,7 +2,7 @@ from fractions import Fraction
 import collections
 import numbers
 
-from pyvmc.core.lattice import Lattice
+from pyvmc.core.lattice import Lattice, LatticeSite
 
 periodic = 1
 antiperiodic = Fraction(1, 2)
@@ -14,7 +14,7 @@ def valid_boundary_conditions(boundary_conditions, n_dimensions):
                 all(isinstance(bc, numbers.Real) and bc != 0
                     for bc in boundary_conditions))
 
-def enforce_boundary(bravais_site, lattice, boundary_conditions=None):
+def enforce_boundary(site, lattice, boundary_conditions=None):
     """Enforce the boundary of a site which may be outside the lattice
 
     Any quantum amplitudes should be multiplied by
@@ -22,14 +22,18 @@ def enforce_boundary(bravais_site, lattice, boundary_conditions=None):
     """
     assert isinstance(lattice, Lattice)
     lattice_dimensions = lattice.dimensions
+    is_LatticeSite = isinstance(site, LatticeSite)
+    bravais_site = site.bs if is_LatticeSite else site
     assert isinstance(bravais_site, collections.Sequence)
     assert len(bravais_site) == len(lattice_dimensions)
     assert all(isinstance(x, numbers.Integral) for x in bravais_site)
     new_bravais_site = tuple(x % length for x, length in zip(bravais_site, lattice_dimensions))
+    new_site = LatticeSite(new_bravais_site, site.bi) if is_LatticeSite else new_bravais_site
+    assert (not is_LatticeSite) or new_site in lattice
     if boundary_conditions is None:
-        return new_bravais_site
+        return new_site
     else:
         assert valid_boundary_conditions(boundary_conditions, len(lattice_dimensions))
         phase_adjustment = sum((x // length) * bc for x, length, bc
                                in zip(bravais_site, lattice_dimensions, boundary_conditions)) % 1
-        return new_bravais_site, phase_adjustment
+        return new_site, phase_adjustment
--- pyvmc/tests/core/test_boundary_conditions.py
+++ pyvmc/tests/core/test_boundary_conditions.py
@@ -1,6 +1,8 @@
 from fractions import Fraction
 
-from pyvmc.core.lattice import Lattice
+import pytest
+
+from pyvmc.core.lattice import Lattice, LatticeSite
 from pyvmc.core.boundary_conditions import periodic, antiperiodic, enforce_boundary
 
 def test_boundary_condition_definitions():
@@ -9,13 +11,25 @@ def test_boundary_condition_definitions():
 
 def test_enforce_boundary_without_boundary_conditions():
     lattice = Lattice([24, 4])
+
+    # BravaisSite
     assert enforce_boundary((24, 3), lattice) == (0, 3)
     assert enforce_boundary((-1, 4), lattice) == (23, 0)
 
+    # LatticeSite
+    assert enforce_boundary(LatticeSite((-1, 0)), lattice) == LatticeSite((23, 0))
+    with pytest.raises(AssertionError):
+        enforce_boundary(LatticeSite((-1, 0), 1), lattice)
+
 def test_enforce_boundary_with_boundary_conditions():
-    lattice = Lattice([8, 8])
+    lattice = Lattice([8, 8], 2)
+
+    # BravaisSite
     assert enforce_boundary((4, 4), lattice, (periodic, periodic)) == ((4, 4), 0)
     assert enforce_boundary((4, 4), lattice, (antiperiodic, antiperiodic)) == ((4, 4), 0)
     assert enforce_boundary((13, 4), lattice, (periodic, periodic)) == ((5, 4), 0)
     assert enforce_boundary((13, 4), lattice, (antiperiodic, periodic)) == ((5, 4), Fraction(1, 2))
     assert enforce_boundary((-3, 4), lattice, (antiperiodic, periodic)) == ((5, 4), Fraction(1, 2))
+
+    # LatticeSite
+    assert enforce_boundary(LatticeSite((-3, 4), 1), lattice, (antiperiodic, periodic)) == (LatticeSite([5, 4], 1), Fraction(1, 2))

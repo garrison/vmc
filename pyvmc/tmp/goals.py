a2b12f1eaa244bf199c3cbf9da865ed60d00d4aa
Jim Garrison <garrison@physics.ucsb.edu>
2012-05-20 23:49:33 -0700

    added the goals/universe system
    I'm not 100% satisfied with how these are currently structured, so I put them in
    pyvmc.tmp.  Since I'm using them all over the place (and since some tests depend
    on them) I figured it made sense to add them to the repository, even though they
    will be significantly refactored at some later date.

---
+++ pyvmc/tmp/goals.py
@@ -0,0 +1,238 @@
+from twisted.internet import defer
+
+from pyvmc.core import SimpleSubsystem
+from pyvmc.core.measurement import SubsystemOccupationProbabilityMeasurementPlan
+from pyvmc.library.renyi import RenyiSignMeasurementPlan, RenyiModPossibleMeasurementPlan
+from pyvmc.tmp.universe import TmpMeasurementPlan, get_default_universe
+
+# there are two types of goals: ones which are measured directly in the VMC,
+# and ones which depend on other goals
+
+#fixme
+def json_to_complex(value):
+    assert len(value) == 2
+    return value[0] + 1j * value[1]
+
+class Goal(object):
+    def __init__(self, independent, universe):
+        self.n_independent = independent
+        self.universe = universe
+
+class SubsystemOccupationNumberProbability(Goal):
+    def __init__(self, system, subsystem, independent=30, universe=None):
+        if universe is None:
+            universe = get_default_universe()
+
+        measurement = SubsystemOccupationProbabilityMeasurementPlan(system, subsystem)
+        plan = TmpMeasurementPlan(measurement, self.parse_result)
+        self.measurement_set = universe.get_measurement_set(plan, independent)
+
+        self.subsystem = subsystem
+
+    def advance(self):
+        return self.measurement_set.advance()
+
+    @staticmethod
+    def parse_result(result_json):
+        rv = {}
+        for occupation, probability in result_json:
+            occupation = tuple(occupation)
+            rv[occupation] = probability
+        return rv
+
+    def get_swap_probability(self, index, n_swaps=2):
+        return sum(v ** n_swaps for v in self.measurement_set[index].get_aggregate_result().values())
+
+class RenyiModDirect(Goal):
+    def __init__(self, system, subsystem, independent=30, universe=None):
+        if universe is None:
+            universe = get_default_universe()
+
+        measurement = RenyiModMeasurementPlan(wavefunction, subsystem)
+        plan = TmpMeasurementPlan(measurement, self.parse_result)
+        self.measurement_set = universe.get_measurement_set(plan, independent)
+
+        self.subsystem = subsystem
+
+    def advance(self):
+        return self.measurement_set.advance()
+
+    @staticmethod
+    def parse_result(result_json):
+        assert isinstance(result_json, float)
+        return result_json
+
+    def get_renyi_mod(self, index):
+        # fixme: we might be taking the log of zero, which is a non-fatal error
+        # (we just need to run longer), but which will kill things ...
+        from math import log
+        return -log(self.measurement_set[index].get_aggregate_result())
+
+class RenyiModPossible(Goal):
+    def __init__(self, system, subsystem, independent=30, universe=None):
+        if universe is None:
+            universe = get_default_universe()
+
+        measurement = RenyiModPossibleMeasurementPlan(system, subsystem)
+        plan = TmpMeasurementPlan(measurement, self.parse_result)
+        self.measurement_set = universe.get_measurement_set(plan, independent)
+
+        self.subsystem = subsystem
+
+    def advance(self):
+        return self.measurement_set.advance()
+
+    @staticmethod
+    def parse_result(result_json):
+        assert isinstance(result_json, float)
+        return result_json
+
+    def get_renyi_mod_possible(self, index):
+        from math import log
+        return -log(self.measurement_set[index].get_aggregate_result())
+
+class RenyiSign(Goal):
+    def __init__(self, system, subsystem, independent=30, universe=None):
+        if universe is None:
+            universe = get_default_universe()
+
+        measurement = RenyiSignMeasurementPlan(system, subsystem)
+        plan = TmpMeasurementPlan(measurement, self.parse_result)
+        self.measurement_set = universe.get_measurement_set(plan, independent)
+
+        self.subsystem = subsystem
+
+    def advance(self):
+        return self.measurement_set.advance()
+
+    @staticmethod
+    def parse_result(result_json):
+        return json_to_complex(result_json)
+
+    def get_renyi_sign(self, index):
+        from math import log
+        try:
+            return -log(self.measurement_set[index].get_aggregate_result().real)
+        except Exception:
+            import logging
+            # if this turns up negative, then the orbitals probably aren't
+            # normalized correct (or some over/under flow is happening)
+            logging.error("domain error %s", self.measurement_set[index].get_aggregate_result().real)
+            return 0.0
+
+class RenyiMod(Goal):
+    def __init__(self, system, subsystem, independent=30, universe=None):
+        self.renyi_mod_possible = RenyiModPossible(system, subsystem, independent, universe)
+        self.sonp = SubsystemOccupationNumberProbability(system, subsystem, independent, universe)
+        self.subsystem = subsystem
+
+    def advance(self):
+        d1 = self.renyi_mod_possible.advance()
+        d2 = self.sonp.advance()
+        d = defer.gatherResults([d1, d2], consumeErrors=True)
+        return d
+
+    def get_renyi_mod(self, index):
+        return self.get_renyi_mod_possible(index) + self.get_renyi_swap_possible(index)
+
+    def get_renyi_swap_possible(self, index):
+        from math import log
+        # fixme: could be taking log of zero ...
+        return -log(self.sonp.get_swap_probability(index))
+
+    def get_renyi_mod_possible(self, index):
+        return self.renyi_mod_possible.get_renyi_mod_possible(index)
+
+class Renyi(Goal):
+    """given a system and a subsystem size, we want the renyi entropy of it"""
+
+    def __init__(self, system, subsystem, independent=30, universe=None):
+        self.renyi_sign = RenyiSign(system, subsystem, independent, universe)
+        self.renyi_mod = RenyiMod(system, subsystem, independent, universe)
+        self.subsystem = subsystem
+
+    def advance(self):
+        """Returns a deferred"""
+        # fixme: handle correctly if an advancement is already queued
+        d1 = self.renyi_mod.advance()
+        d2 = self.renyi_sign.advance()
+        d = defer.gatherResults([d1, d2], consumeErrors=True)
+        # we might want to add a callback that ... tells us to clear the cache :)
+        return d
+
+    def get_renyi_sign(self, index):
+        return self.renyi_sign.get_renyi_sign(index)
+
+    def get_renyi_mod(self, index):
+        return self.renyi_mod.get_renyi_mod(index)
+
+    def get_renyi(self, index):
+        return self.get_renyi_sign(index) + self.get_renyi_mod(index)
+
+class RenyiLengthScaling(Goal):
+    """given a quasi-1d system, we want to figure out c based on the renyi entropies"""
+
+    def __init__(self, system, renyi_class=Renyi, independent=30, universe=None):
+        subsystem_lengths = xrange(1, (system.lattice.dimensions[0] + 1) // 2 + 1)
+        subsystems = [SimpleSubsystem((x,) + system.lattice.dimensions[1:], system.lattice)
+                      for x in subsystem_lengths]
+        self.renyi_plans = [renyi_class(system, subsystem, independent, universe)
+                            for subsystem in subsystems]
+        self.system = system
+
+    def advance(self):
+        """Returns a deferred"""
+        # fixme: handle correctly if an advancement is already queued
+        deferreds = [r.advance() for r in self.renyi_plans]
+        assert len(deferreds) > 0  # i'm not sure if it works if there are zero...
+        return defer.gatherResults(deferreds, consumeErrors=True)
+
+#    def _get_c(self):
+#        class A(object):
+#            pass
+#        rv = A()
+#        rv.value = '373'
+#        return rv
+#
+#    c = property(_get_c, doc="conformal charge c")
+
+    def get_c(self, index):
+        system_length = self.system.lattice.dimensions[0]
+        def conformal_length(x):
+            from math import pi, log, sin
+            return log(system_length / pi * sin(pi * x / system_length))
+        conformal_lengths = [conformal_length(p.subsystem.dimensions[0]) for p in self.renyi_plans]
+        renyi_entropies = [p.get_renyi(index) for p in self.renyi_plans]
+
+        import numpy
+        fit = numpy.polyfit(conformal_lengths, renyi_entropies, 1)
+        return 4.0 * fit[0]
+
+class DensityDensity(Goal):
+    @staticmethod
+    def parse_result(result_json):
+        rv = {}
+        assert isinstance(result_json, list)
+        for basis_index, bi_result in enumerate(result_json):
+            assert isinstance(bi_result, list)
+            for position_index, value in enumerate(bi_result):
+                assert isinstance(value, float)
+                rv[Point(basis_index, position_index)] = value
+        return rv
+
+class Green(Goal):
+    @staticmethod
+    def parse_result(result_json):
+        rv = {}
+        assert isinstance(result_json, list)
+        for basis_index, bi_result in enumerate(result_json):
+            assert isinstance(bi_result, list)
+            for position_index, value in enumerate(bi_result):
+                rv[Point(basis_index, position_index)] = json_to_complex(value)
+        return rv
+
+class DensityDensityFourier(Goal):
+    pass
+
+class GreenFourier(Goal):
+    pass
---
+++ pyvmc/tmp/universe.py
@@ -0,0 +1,258 @@
+from __future__ import division
+
+# fixme: remove need for copy, deepcopy, and hashable_json
+
+from pyvmc.utils import complex_json as json
+import random
+from copy import deepcopy, copy
+import logging
+
+from twisted.internet import defer, task, reactor
+
+# fixme: or we pass a scheduler in advance()
+from pyvmc.control.scheduler import default_scheduler
+
+logger = logging.getLogger(__name__)
+
+def hashable_json(obj):
+    if isinstance(obj, dict):
+        return frozenset((k, hashable_json(v)) for k, v in obj.iteritems())
+    elif isinstance(obj, list):
+        return tuple(hashable_json(a) for a in obj)
+    else:
+        return obj
+
+def get_vmc_core_path():
+    import os
+    try:
+        return os.environ["VMC_CORE_PATH"]
+    except KeyError:
+        # look in ../../vmc-core/vmc-core
+        return os.path.join(os.path.dirname(__file__), "..", "..", "vmc-core", "vmc-core")
+
+class TmpMeasurementPlan(object):
+    def __init__(self, measurement_plan, parse_result):
+        self.walk = {}
+        self.walk["system"] = deepcopy(measurement_plan.walk.wavefunction.to_json())
+        self.walk["simulation"] = deepcopy(measurement_plan.walk.to_json())
+        self.measurement = deepcopy(measurement_plan.to_json())
+        self.hashable = hashable_json([self.walk, measurement_plan])
+        self.hashable_walk = hashable_json(self.walk)
+        self.parse_result = parse_result  # fixme (?)
+
+class Walk(object):
+    equilibrium = False
+
+    def __init__(self, walk_json):
+        self.walk_json = deepcopy(walk_json)
+        self.walk_json["simulation"]["equilibrium-steps"] = 500000
+        self.walk_json["simulation"]["measurement-steps"] = 500000
+        self.measurements_in_progress = []
+        self.measurements_pending = []
+        self.waiting_on_walk = False
+
+    def _advancement_completed(self, args):
+        output, err = args
+        output = json.loads(output)
+        logger.info("monte carlo stats: %s", output["monte-carlo-stats"])
+        self.walk_json["simulation"]["equilibrium-steps"] = 0
+        self.walk_json["simulation"]["initial-positions"] = output["final-positions"]
+        for result_json, (measurement, deferred) in zip(output["measurements"], self.measurements_in_progress):
+            deferred.callback((result_json, self.walk_json["simulation"]["measurement-steps"]))
+        del self.measurements_in_progress[:]
+        if self.measurements_pending:
+            reactor.callLater(0, self._advance_pending_measurements)
+            assert not self.waiting_on_walk
+            self.waiting_on_walk = True
+
+    def _advancement_failed(self, args):
+        for measurement, deferred in self.measurements_in_progress:
+            deferred.errback()
+        del self.measurements_in_progress[:]
+
+    def _advance_pending_measurements(self):
+        self.waiting_on_walk = False
+        self.measurements_in_progress = self.measurements_pending
+        self.measurements_pending = []
+        vmc_core_input = copy(self.walk_json)
+        vmc_core_input["simulation"] = copy(vmc_core_input["simulation"])
+        vmc_core_input["simulation"]["measurements"] = [m[0].measurement_plan.measurement
+                                                        for m in self.measurements_in_progress]
+        vmc_core_input["rng"] = { "seed": random.randint(0, 2 ** 32 - 1) }
+        d = default_scheduler.submit(get_vmc_core_path(), json.dumps(vmc_core_input))
+        d.addCallbacks(self._advancement_completed, self._advancement_failed)
+
+    def advance_measurement(self, measurement):
+        assert (measurement not in self.measurements_pending)
+        deferred = defer.Deferred()
+        self.measurements_pending.append((measurement, deferred))
+        if not self.measurements_in_progress and not self.waiting_on_walk:
+            reactor.callLater(0, self._advance_pending_measurements)
+        self.waiting_on_walk = True
+        return deferred
+
+class WalkSet(object):
+    def __init__(self, walk_json, n_independent):
+        # walk_json has already been deepcopy'd, so we can just store a
+        # reference to it and it shouldn't change.
+        self.walk_json = walk_json
+        self.ws = [Walk(walk_json) for i in xrange(n_independent)]
+
+    def extend_by(self, n):
+        self.ms.extend(Walk(self.walk_json) for i in xrange(n))
+
+    def extend_to(self, n):
+        if n > len(self.ws):
+            self.extend_by(n - len(self.ws))
+
+    def __len__(self):
+        return len(self.ws)
+
+    def __iter__(self):
+        return iter(self.ws)
+
+    def __getitem__(self, index):
+        return self.ws[index]
+
+class Measurement(object):
+    def __init__(self, measurement_plan):
+        self.measurement_plan = measurement_plan
+        self.result = None
+        self.total_measurements = 0
+
+    def _record_result(self, args):
+        self._add_result(*args)
+        return None
+
+    def advance(self, walk):
+        d = walk.advance_measurement(self)
+        d.addCallback(self._record_result)
+        return d
+
+    def _add_result(self, result_json, n_measurements):
+        result = self.measurement_plan.parse_result(result_json)
+        if self.result is None:
+            if isinstance(result, dict):
+                selfresult = {}
+                for k, v in result.iteritems():
+                    selfresult[k] = v * n_measurements
+                self.result = selfresult
+            else:
+                self.result = result * n_measurements
+        else:
+            if isinstance(result, dict):
+                selfresult = self.result
+                assert isinstance(selfresult, dict)
+                keys = set(selfresult)
+                keys.update(result)
+                for k in keys:
+                    selfresult[k] = selfresult.get(k, 0.0) + result.get(k, 0.0) * n_measurements
+            else:
+                self.result += result * n_measurements
+
+        self.total_measurements += n_measurements
+
+    def get_aggregate_result(self):
+        # fixme: cache this until it changes
+        selfresult = self.result
+        total_measurements = self.total_measurements
+
+        if total_measurements == 0:
+            # or we could just return None here...
+            raise Exception("no measurements have been added.  cannot take aggregate result.")
+
+        if isinstance(selfresult, dict):
+            rv = {}
+            for k, v in selfresult.iteritems():
+                # fixme: we might want to perform some transformation on the
+                # keys, for instance, to turn them into floats instead of
+                # indices for k vectors.  maybe a generic mechanism here?  call
+                # a classmethod that by default just returns the value...
+                #
+                # but then again, we will probably do more on this, such as
+                # error analysis based on the key, so it is probably best just
+                # to keep this elsewhere.
+                rv[k] = v / total_measurements
+            return rv
+        else:
+            return selfresult / total_measurements
+
+class MeasurementSet(object):
+    """A set of identical, actual measurements taken directly from independent monte carlo runs"""
+
+    def __init__(self, measurement_plan, walk_set, n_independent):
+        self.measurement_plan = measurement_plan
+        self.walk_set = walk_set
+        self.ms = [Measurement(measurement_plan) for i in xrange(n_independent)]
+        self.advancement_in_progress_deferreds = []  # blank if not advancement is in progress
+
+    def extend_by(self, n):
+        self.ms.extend(Measurement(self.measurement_plan) for i in xrange(n))
+        self.walk_set.extend_to(len(self.ms))
+
+    def extend_to(self, n):
+        if n > len(self.ms):
+            self.extend_by(n - len(self.ms))
+
+    def __len__(self):
+        return len(self.ms)
+
+    def __iter__(self):
+        return iter(self.ms)
+
+    def __getitem__(self, index):
+        return self.ms[index]
+
+    def _advancement_completed(self, args):
+        for deferred in self.advancement_in_progress_deferreds:
+            deferred.callback(None)
+        del self.advancement_in_progress_deferreds[:]
+
+    def _advancement_failed(self, failure):
+        for deferred in self.advancement_in_progress_deferreds:
+            deferred.errback(failure)
+        del self.advancement_in_progress_deferreds[:]
+
+    def _advance(self):
+        deferreds = [m.advance(self.walk_set[i]) for i, m in enumerate(self.ms)]
+        return defer.gatherResults(deferreds, consumeErrors=True)
+
+    def advance(self):
+        deferred = defer.Deferred()
+        if not self.advancement_in_progress_deferreds:
+            self._advance().addCallbacks(self._advancement_completed,
+                                         self._advancement_failed)
+        self.advancement_in_progress_deferreds.append(deferred)
+        return deferred
+
+class Universe(object):
+    def __init__(self):
+        # fixme: i bet these could even be weakref dictionaries
+        self.walksets = {}
+        self.measurementsets = {}
+
+    def _get_walk_set(self, measurement_plan, n_independent):
+        try:
+            walk_set = self.walksets[measurement_plan.hashable_walk]
+        except KeyError:
+            walk_set = WalkSet(measurement_plan.walk, n_independent)
+            self.walksets[measurement_plan.hashable_walk] = walk_set
+        else:
+            walk_set.extend_to(n_independent)
+        return walk_set
+
+    def get_measurement_set(self, measurement_plan, n_independent):
+        try:
+            measurement_set = self.measurementsets[measurement_plan.hashable]
+        except KeyError:
+            walk_set = self._get_walk_set(measurement_plan, n_independent)
+            measurement_set = MeasurementSet(measurement_plan, walk_set, n_independent)
+            self.measurementsets[measurement_plan.hashable] = measurement_set
+        else:
+            measurement_set.extend_to(n_independent)
+        return measurement_set
+
+global_universe = Universe()
+
+def get_default_universe():
+    return global_universe

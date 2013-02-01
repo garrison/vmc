7e50aaf5297594f93a2d3205a6584c56c2f71372
Jim Garrison <garrison@physics.ucsb.edu>
2013-02-01 02:24:22 -0800

    added is_hermitian() function

--- pyvmc/utils/sparse.py
+++ pyvmc/utils/sparse.py
@@ -8,3 +8,14 @@ def sparse_absolute(mat):
         for j in range(mat.shape[1]):
             mat[i, j] = abs(mat[i, j])
     return mat
+
+def is_hermitian(mat):
+    # For dense matrices you can simply test (mat == mat.H), but for some
+    # reason this always returns False for sparse matrices.
+    if mat.shape[0] != mat.shape[1]:
+        return False
+    for i in range(mat.shape[0]):
+        for j in range(i, mat.shape[1]):
+            if mat[(i, j)] != mat[(j, i)].conjugate():
+                return False
+    return True

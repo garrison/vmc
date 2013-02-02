a76e266af2b4ebb21fcc8999f495279ec0f253ff
Jim Garrison <garrison@physics.ucsb.edu>
2013-02-02 14:59:14 -0800

    fixing up the boundary condition tests

--- pyvmc/core/boundary_conditions.pyx
+++ pyvmc/core/boundary_conditions.pyx
@@ -2,6 +2,8 @@ from fractions import Fraction
 import collections
 import numbers
 
+from pyvmc.core.lattice cimport MAX_DIMENSION
+
 periodic_bc = periodic = 1
 antiperiodic_bc = antiperiodic = Fraction(1, 2)
 open_bc = 0
@@ -13,13 +15,16 @@ def boundary_condition_to_string(bc):
         open_bc: 'open',
     }.get(bc, repr(bc))
 
+def valid_boundary_condition(bc):
+    return bool(isinstance(bc, numbers.Rational) and 0 <= bc <= 1)
+
 def valid_boundary_conditions(boundary_conditions, n_dimensions):
-    assert isinstance(n_dimensions, numbers.Integral) and n_dimensions > 0
+    assert isinstance(n_dimensions, numbers.Integral) and 0 < n_dimensions <= MAX_DIMENSION
     return bool(isinstance(boundary_conditions, collections.Sequence) and
                 len(boundary_conditions) == n_dimensions and
-                all(isinstance(bc, numbers.Real) and 0 <= bc <= 1
+                all(valid_boundary_condition(bc)
                     for bc in boundary_conditions))
 
 def valid_closed_boundary_conditions(boundary_conditions, n_dimensions):
     return bool(valid_boundary_conditions(boundary_conditions, n_dimensions) and
-                all(bc != 0 for bc in boundary_conditions))
+                all(bc != open_bc for bc in boundary_conditions))

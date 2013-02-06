666087b98087ce82b15d0b739347751dfe50fa98
Jim Garrison <garrison@physics.ucsb.edu>
2013-02-05 22:12:09 -0800

    added some docstrings to parameter_iteration.py

--- pyvmc/utils/parameter_iteration.py
+++ pyvmc/utils/parameter_iteration.py
@@ -1,3 +1,6 @@
+"""Tools for iterating over a set of parameters (e.g. relative energy scales)
+"""
+
 from collections import OrderedDict
 from itertools import product
 from math import sqrt
@@ -26,5 +29,29 @@ def iterate_values(n_parameters, n_steps=None, exp_func=None):
             if 0 in a)
 
 def iterate_parameters(parameters, n_steps=None, exp_func=None):
+    """Iterate over some set of dimensionful parameters.
+
+    This assumes that the states are equivalent if rescaled by a common factor.
+    For instance, we don't consider the state where all values are equal to 2,
+    as this is equivalent to the state where all values are equal to 1.
+
+    This also skips the state where all values are zero.
+
+    A dict is returned for each state.  The keys of this dictionary are taken
+    from the list of `parameters` given.
+
+    >>> for parameters in iterate_parameters(['t', 'J'], n_steps=5):
+    ...  print('t = {t:.3f} ; J = {J:.3f}'.format(**parameters))
+    ...
+    t = 0.000 ; J = 1.000
+    t = 1.000 ; J = 0.000
+    t = 1.000 ; J = 1.000
+    t = 1.000 ; J = 1.414
+    t = 1.000 ; J = 2.000
+    t = 1.000 ; J = 2.828
+    t = 1.414 ; J = 1.000
+    t = 2.000 ; J = 1.000
+    t = 2.828 ; J = 1.000
+    """
     return (OrderedDict(zip(parameters, values))
             for values in iterate_values(len(parameters), n_steps, exp_func))

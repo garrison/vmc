d1c92d1d4feaa5637e0253b18575a57028d210ea
Jim Garrison <garrison@physics.ucsb.edu>
2013-02-11 23:19:02 -0800

    lowered the Ceperley determinant cutoff

--- vmc-core/CeperleyMatrix.cpp
+++ vmc-core/CeperleyMatrix.cpp
@@ -1,7 +1,7 @@
 #include "CeperleyMatrix.hpp"
 
 template<>
-const std::complex<double> CeperleyMatrix<std::complex<double> >::ceperley_determinant_cutoff = .001;
+const std::complex<double> CeperleyMatrix<std::complex<double> >::ceperley_determinant_cutoff = 1e-6;
 
 template<>
-const std::complex<long double> CeperleyMatrix<std::complex<long double> >::ceperley_determinant_cutoff = .001;
+const std::complex<long double> CeperleyMatrix<std::complex<long double> >::ceperley_determinant_cutoff = 1e-6;

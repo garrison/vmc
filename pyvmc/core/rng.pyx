0730430db60b81be23371605763bb352086ff362
Jim Garrison <garrison@physics.ucsb.edu>
2013-02-14 02:17:25 -0800

    [py3k] make RandomNumberGenerator work if given a unicode string

--- pyvmc/core/rng.pyx
+++ pyvmc/core/rng.pyx
@@ -2,14 +2,15 @@ from random import randint
 
 cdef class RandomNumberGenerator(object):
     def __init__(self, name="boost::mt19937", seed=None):
-        if not rng_name_is_valid(name):
+        namebytes = name.encode("ascii")
+        if not rng_name_is_valid(namebytes):
             raise ValueError("invalid RNG: {}".format(name))
         self.name = name
         if seed is None:
             self.seed = randint(0, <unsigned long>(-1))
         else:
             self.seed = seed
-        self.autoptr = create_rng(self.name, self.seed)
+        self.autoptr = create_rng(namebytes, self.seed)
 
     def is_good(self):
         return self.autoptr.get() is not NULL

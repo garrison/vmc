76af75951e04777f4e7dba75e55b709376b6a129
Jim Garrison <garrison@physics.ucsb.edu>
2013-02-15 01:02:10 -0800

    changed some notation in bcs_tests.py

--- pyvmc/mc_tests/bcs_tests.py
+++ pyvmc/mc_tests/bcs_tests.py
@@ -8,11 +8,11 @@ from pyvmc.utils import average
 
 logger = logging.getLogger(__name__)
 
-def test_ansatz(lattice, chi, eta, a0_3, expected_results, tolerance):
+def test_ansatz(lattice, t1, delta1, mu0, expected_results, tolerance):
     parton_boundary_conditions = (periodic, antiperiodic)
 
     from pyvmc.library.mft_utils import did_hf_bcs_theory
-    phi = did_hf_bcs_theory(lattice, parton_boundary_conditions, t1=chi, delta1=eta, mu0=a0_3, delta0=0)['pairing_matrix']
+    phi = did_hf_bcs_theory(lattice, parton_boundary_conditions, t1=t1, delta1=delta1, mu0=mu0, delta0=0)['pairing_matrix']
 
     from pyvmc.library.bcs import ProjectedBCSWavefunction
     wf = ProjectedBCSWavefunction(**{
@@ -37,7 +37,7 @@ def test_ansatz(lattice, chi, eta, a0_3, expected_results, tolerance):
         evaluator(J1=0, J2=0, J3=1, K=0) / len(lattice) / 3,
         evaluator(J1=0, J2=0, J3=0, K=1) / len(lattice) / 3,
     )
-    logger.info("chi=%f, eta=%f, a0_3=%f", chi, eta, a0_3)
+    logger.info("t1=%f, delta1=%f, mu0=%f", t1, delta1, mu0)
     logger.info("  nn: %f", final_results[0])
     logger.info(" 2nn: %f", final_results[1])
     logger.info(" 3nn: %f", final_results[2])
@@ -47,15 +47,15 @@ def test_ansatz(lattice, chi, eta, a0_3, expected_results, tolerance):
 
 def test_projected_bcs_states(tolerance=.015):
     # Z2A
-    test_ansatz(HexagonalLattice([6, 6]), chi=0, eta=1, a0_3=0,
+    test_ansatz(HexagonalLattice([6, 6]), t1=0, delta1=1, mu0=0,
                 expected_results=(-.1589, .0266, .0279, -.09), tolerance=tolerance)
-    #test_ansatz(HexagonalLattice([18, 18]), chi=0, eta=1, a0_3=0,
+    #test_ansatz(HexagonalLattice([18, 18]), t1=0, delta1=1, mu0=0,
     #            expected_results=(-.157, .028, .025, -.10), tolerance=tolerance)
 
     # ChSU2B
-    test_ansatz(HexagonalLattice([6, 6]), chi=1, eta=sqrt(2), a0_3=0,
+    test_ansatz(HexagonalLattice([6, 6]), t1=1, delta1=sqrt(2), mu0=0,
                 expected_results=(-.1604, .029, .017, .102), tolerance=tolerance)
-    #test_ansatz(HexagonalLattice([18, 18]), chi=1, eta=sqrt(2), a0_3=0,
+    #test_ansatz(HexagonalLattice([18, 18]), t1=1, delta1=sqrt(2), mu0=0,
     #            expected_results=(-.160, .029, .017, .102), tolerance=tolerance)
 
 if __name__ == "__main__":

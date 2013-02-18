b7575a4f620ee39b0606d88f464cded2b6ce689f
Jim Garrison <garrison@physics.ucsb.edu>
2013-02-18 11:03:49 -0800

    we now store and calculate the hermitian conjugate of each operator instead of inferring it

--- pyvmc/operators/__init__.py
+++ pyvmc/operators/__init__.py
@@ -1,7 +1,13 @@
 from pyvmc.core.lattice import Lattice, LatticeSite
 from pyvmc.core.boundary_conditions import valid_boundary_conditions
 from pyvmc.core.operator import SiteHop, BasicOperator, CompositeOperator
-from pyvmc.utils import add_hc, ensure_real
+from pyvmc.utils import ensure_real
+
+if True:
+    def soft_ensure_hermitian(x):
+        return x.real
+else:
+    soft_ensure_hermitian = ensure_real
 
 class DensityDensityOperator(CompositeOperator):
     __slots__ = ("operators",)
@@ -36,6 +42,7 @@ class SpinSpinOperator(CompositeOperator):
                 BasicOperator([SiteHop(site1, site1, 0), SiteHop(site2, site2, 1)], boundary_conditions),
                 BasicOperator([SiteHop(site1, site1, 1), SiteHop(site2, site2, 0)], boundary_conditions),
                 BasicOperator([SiteHop(site1, site2, 0), SiteHop(site2, site1, 1)], boundary_conditions),
+                BasicOperator([SiteHop(site1, site2, 1), SiteHop(site2, site1, 0)], boundary_conditions),
             )
             return (operators, False)
 
@@ -46,7 +53,7 @@ class SpinSpinOperator(CompositeOperator):
                 return .75 * (context[operators[0]] + context[operators[1]])  # i.e. .75 * rho, scaled by number of sites if summing
             else:
                 return ensure_real(
-                    -.5 * add_hc(context[operators[4]]) +
+                    -.5 * soft_ensure_hermitian(context[operators[4]] + context[operators[5]]) +
                     +.25 * context[operators[0]] +
                     +.25 * context[operators[1]] +
                     -.25 * context[operators[2]] +
@@ -65,12 +72,17 @@ class SingletRingExchangeOperator(CompositeOperator):
             BasicOperator([SiteHop(LatticeSite((0, 0)), LatticeSite((1, 0)), 1), SiteHop(LatticeSite((1, 1)), LatticeSite((0, 1)), 0)], boundary_conditions),
             BasicOperator([SiteHop(LatticeSite((0, 0)), LatticeSite((0, 1)), 0), SiteHop(LatticeSite((1, 1)), LatticeSite((1, 0)), 1)], boundary_conditions),
             BasicOperator([SiteHop(LatticeSite((0, 0)), LatticeSite((0, 1)), 1), SiteHop(LatticeSite((1, 1)), LatticeSite((1, 0)), 0)], boundary_conditions),
+            # hc
+            BasicOperator([SiteHop(LatticeSite((1, 0)), LatticeSite((0, 0)), 0), SiteHop(LatticeSite((0, 1)), LatticeSite((1, 1)), 1)], boundary_conditions),
+            BasicOperator([SiteHop(LatticeSite((1, 0)), LatticeSite((0, 0)), 1), SiteHop(LatticeSite((0, 1)), LatticeSite((1, 1)), 0)], boundary_conditions),
+            BasicOperator([SiteHop(LatticeSite((0, 1)), LatticeSite((0, 0)), 0), SiteHop(LatticeSite((1, 0)), LatticeSite((1, 1)), 1)], boundary_conditions),
+            BasicOperator([SiteHop(LatticeSite((0, 1)), LatticeSite((0, 0)), 1), SiteHop(LatticeSite((1, 0)), LatticeSite((1, 1)), 0)], boundary_conditions),
         )
         return (operators,)
 
     def evaluate(self, context):
         def _evaluate():
-            return .5 * sum([o.evaluate(context)() for o in self.operators])
+            return .5 * soft_ensure_hermitian(sum([o.evaluate(context)() for o in self.operators]))
         return _evaluate
 
 class TJKHamiltonian(CompositeOperator):
@@ -89,6 +101,11 @@ class TJKHamiltonian(CompositeOperator):
             SpinSpinOperator(LatticeSite((0, 0)), LatticeSite((1, 0)), boundary_conditions),
             SpinSpinOperator(LatticeSite((0, 0)), LatticeSite((0, 1)), boundary_conditions),
             SingletRingExchangeOperator(boundary_conditions),
+            # hc
+            BasicOperator([SiteHop(LatticeSite((1, 0)), LatticeSite((0, 0)), 0)], boundary_conditions),
+            BasicOperator([SiteHop(LatticeSite((1, 0)), LatticeSite((0, 0)), 1)], boundary_conditions),
+            BasicOperator([SiteHop(LatticeSite((0, 1)), LatticeSite((0, 0)), 0)], boundary_conditions),
+            BasicOperator([SiteHop(LatticeSite((0, 1)), LatticeSite((0, 0)), 1)], boundary_conditions),
         )
 
         # don't double-count plaquettes on a 2-leg ladder
@@ -99,8 +116,8 @@ class TJKHamiltonian(CompositeOperator):
 
     def evaluate(self, context):
         operators = self.operators
-        greenx = context[operators[0]] + context[operators[1]]
-        greeny = context[operators[2]] + context[operators[3]]
+        greenx = soft_ensure_hermitian(context[operators[0]] + context[operators[1]] + context[operators[7]] + context[operators[8]])
+        greeny = soft_ensure_hermitian(context[operators[2]] + context[operators[3]] + context[operators[9]] + context[operators[10]])
         spinspinx = operators[4].evaluate(context)()
         spinspiny = operators[5].evaluate(context)()
         ringexchange = operators[6].evaluate(context)()
@@ -117,12 +134,12 @@ class TJKHamiltonian(CompositeOperator):
             if Jperp is None:
                 Jperp = J
 
-            return (
-                -t * add_hc(greenx) / divx +
-                -tperp * add_hc(greeny) / divy +
+            return ensure_real(
+                -t * greenx / divx +
+                -tperp * greeny / divy +
                 J * spinspinx / divx +
                 Jperp * spinspiny / divy +
-                2 * K * add_hc(ringexchange) / divx / divy
+                2 * K * ringexchange / divx / divy
             )
 
         return _evaluate
@@ -139,8 +156,11 @@ class JKHamiltonian(CompositeOperator):
         assert valid_boundary_conditions(boundary_conditions, len(lattice.dimensions))
         operators = (
             BasicOperator([SiteHop(LatticeSite((0, 0)), LatticeSite((1, 0)), 0)], boundary_conditions),
+            BasicOperator([SiteHop(LatticeSite((1, 0)), LatticeSite((0, 0)), 0)], boundary_conditions),
             BasicOperator([SiteHop(LatticeSite((0, 0)), LatticeSite((0, 1)), 0)], boundary_conditions),
+            BasicOperator([SiteHop(LatticeSite((0, 1)), LatticeSite((0, 0)), 0)], boundary_conditions),
             BasicOperator([SiteHop(LatticeSite((0, 0)), LatticeSite((0, 1)), 0), SiteHop(LatticeSite((1, 1)), LatticeSite((1, 0)), 0)], boundary_conditions),
+            BasicOperator([SiteHop(LatticeSite((1, 0)), LatticeSite((1, 1)), 0), SiteHop(LatticeSite((0, 1)), LatticeSite((0, 0)), 0)], boundary_conditions),
         )
 
         # don't double-count plaquettes on a 2-leg ladder
@@ -151,9 +171,9 @@ class JKHamiltonian(CompositeOperator):
 
     def evaluate(self, context):
         operators = self.operators
-        greenx = add_hc(context[operators[0]])
-        greeny = add_hc(context[operators[1]])
-        ringexchange = add_hc(operators[2].evaluate(context)())
+        greenx = soft_ensure_hermitian(context[operators[0]] + context[operators[1]])
+        greeny = soft_ensure_hermitian(context[operators[2]] + context[operators[3]])
+        ringexchange = soft_ensure_hermitian(operators[4].evaluate(context)() + operators[5].evaluate(context)())
 
         divx = self.divx
         divy = self.divy
@@ -184,12 +204,15 @@ class SpinModelRingExchangeOperator(CompositeOperator):
         This is a hermitian operator.
         """
         operators = (
-            # all four spins the same
+            # all four spins the same (these operators are hermitian)
             BasicOperator([SiteHop(site1, site1, 0), SiteHop(site2, site2, 0), SiteHop(site3, site3, 0), SiteHop(site4, site4, 0)], boundary_conditions),
             BasicOperator([SiteHop(site1, site1, 1), SiteHop(site2, site2, 1), SiteHop(site3, site3, 1), SiteHop(site4, site4, 1)], boundary_conditions),
             # two spins up, two spins down (with all spins being changed)
             BasicOperator([SiteHop(site1, site2, 0), SiteHop(site2, site1, 1), SiteHop(site3, site4, 0), SiteHop(site4, site3, 1)], boundary_conditions),
             BasicOperator([SiteHop(site1, site2, 1), SiteHop(site2, site1, 0), SiteHop(site3, site4, 1), SiteHop(site4, site3, 0)], boundary_conditions),
+            # hermitian conjugate
+            BasicOperator([SiteHop(site2, site1, 0), SiteHop(site1, site2, 1), SiteHop(site4, site3, 0), SiteHop(site3, site4, 1)], boundary_conditions),
+            BasicOperator([SiteHop(site2, site1, 1), SiteHop(site1, site2, 0), SiteHop(site4, site3, 1), SiteHop(site3, site4, 0)], boundary_conditions),
             # three spins one way, one the other
             BasicOperator([SiteHop(site4, site4, 0), SiteHop(site1, site1, 0), SiteHop(site2, site3, 0), SiteHop(site3, site2, 1)], boundary_conditions),
             BasicOperator([SiteHop(site4, site4, 1), SiteHop(site1, site1, 1), SiteHop(site2, site3, 1), SiteHop(site3, site2, 0)], boundary_conditions),
@@ -199,17 +222,32 @@ class SpinModelRingExchangeOperator(CompositeOperator):
             BasicOperator([SiteHop(site2, site2, 1), SiteHop(site3, site3, 1), SiteHop(site4, site1, 1), SiteHop(site1, site4, 0)], boundary_conditions),
             BasicOperator([SiteHop(site3, site3, 0), SiteHop(site4, site4, 0), SiteHop(site1, site2, 0), SiteHop(site2, site1, 1)], boundary_conditions),
             BasicOperator([SiteHop(site3, site3, 1), SiteHop(site4, site4, 1), SiteHop(site1, site2, 1), SiteHop(site2, site1, 0)], boundary_conditions),
+            # hermitian conjugate
+            BasicOperator([SiteHop(site4, site4, 0), SiteHop(site1, site1, 0), SiteHop(site3, site2, 0), SiteHop(site2, site3, 1)], boundary_conditions),
+            BasicOperator([SiteHop(site4, site4, 1), SiteHop(site1, site1, 1), SiteHop(site3, site2, 1), SiteHop(site2, site3, 0)], boundary_conditions),
+            BasicOperator([SiteHop(site1, site1, 0), SiteHop(site2, site2, 0), SiteHop(site4, site3, 0), SiteHop(site3, site4, 1)], boundary_conditions),
+            BasicOperator([SiteHop(site1, site1, 1), SiteHop(site2, site2, 1), SiteHop(site4, site3, 1), SiteHop(site3, site4, 0)], boundary_conditions),
+            BasicOperator([SiteHop(site2, site2, 0), SiteHop(site3, site3, 0), SiteHop(site1, site4, 0), SiteHop(site4, site1, 1)], boundary_conditions),
+            BasicOperator([SiteHop(site2, site2, 1), SiteHop(site3, site3, 1), SiteHop(site1, site4, 1), SiteHop(site4, site1, 0)], boundary_conditions),
+            BasicOperator([SiteHop(site3, site3, 0), SiteHop(site4, site4, 0), SiteHop(site2, site1, 0), SiteHop(site1, site2, 1)], boundary_conditions),
+            BasicOperator([SiteHop(site3, site3, 1), SiteHop(site4, site4, 1), SiteHop(site2, site1, 1), SiteHop(site1, site2, 0)], boundary_conditions),
             # two spins up, two spins down (with only two spins being changed)
             BasicOperator([SiteHop(site2, site2, 0), SiteHop(site4, site4, 1), SiteHop(site1, site3, 1), SiteHop(site3, site1, 0)], boundary_conditions),
             BasicOperator([SiteHop(site2, site2, 1), SiteHop(site4, site4, 0), SiteHop(site1, site3, 0), SiteHop(site3, site1, 1)], boundary_conditions),
             BasicOperator([SiteHop(site1, site1, 0), SiteHop(site3, site3, 1), SiteHop(site4, site2, 1), SiteHop(site2, site4, 0)], boundary_conditions),
             BasicOperator([SiteHop(site1, site1, 1), SiteHop(site3, site3, 0), SiteHop(site4, site2, 0), SiteHop(site2, site4, 1)], boundary_conditions),
+            # hermitian conjugate
+            BasicOperator([SiteHop(site2, site2, 0), SiteHop(site4, site4, 1), SiteHop(site3, site1, 1), SiteHop(site1, site3, 0)], boundary_conditions),
+            BasicOperator([SiteHop(site2, site2, 1), SiteHop(site4, site4, 0), SiteHop(site3, site1, 0), SiteHop(site1, site3, 1)], boundary_conditions),
+            BasicOperator([SiteHop(site1, site1, 0), SiteHop(site3, site3, 1), SiteHop(site2, site4, 1), SiteHop(site4, site2, 0)], boundary_conditions),
+            BasicOperator([SiteHop(site1, site1, 1), SiteHop(site3, site3, 0), SiteHop(site2, site4, 0), SiteHop(site4, site2, 1)], boundary_conditions),
         )
         return (operators,)
 
     def evaluate(self, context):
         def _evaluate():
-            assert len(self.operators) == 16
-            return add_hc(sum(o.evaluate(context)() for o in self.operators[0:4])
-                          - sum(o.evaluate(context)() for o in self.operators[4:16]))
+            assert len(self.operators) == 30
+            return soft_ensure_hermitian(2 * sum(o.evaluate(context)() for o in self.operators[0:2])
+                                         + sum(o.evaluate(context)() for o in self.operators[2:6])
+                                         - sum(o.evaluate(context)() for o in self.operators[6:30]))
         return _evaluate

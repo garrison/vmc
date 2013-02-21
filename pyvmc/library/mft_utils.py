a30cc5fbace25677b450973fee2156007e83325b
Ryan Mishmash <mishmash@physics.ucsb.edu>
2013-02-20 19:38:57 -0800

    Just some cleaning up before adding free fermion BCS states

--- pyvmc/library/mft_utils.py
+++ pyvmc/library/mft_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from pyvmc.core import Lattice, LatticeSite, HexagonalLattice, periodic, antiperiodic
+from pyvmc.utils import is_square_matrix
 
 import numpy
 from numpy import pi
@@ -18,8 +19,8 @@ import matplotlib.pyplot as plt
 
 def bcs_soln(t, delta):
     # both t and delta should include on-site terms, e.g., t_{ij} = ttilde_{ij} = t_{ij} + mu_i * krondelta_{ij} from notes
-    assert_ismatrix(t)
-    assert_ismatrix(delta)
+    assert is_square_matrix(t)
+    assert is_square_matrix(delta)
     assert t.shape[0] == delta.shape[0]
 
     Nsites = t.shape[0]
@@ -53,9 +54,9 @@ def bcs_soln(t, delta):
     evalsPos = evals[Nsites:2*Nsites]
     evecsPos = evecs[:, Nsites:2*Nsites]
 
-    logger.debug('positive eigenvalues = ...')
+    logger.debug('Positive eigenvalues = ...')
     logger.debug(evalsPos)
-#    logger.debug('negative eigenvalues = ...')
+#    logger.debug('Negative eigenvalues = ...')
 #    logger.debug(evalsNeg[::-1])
 
     # Check that positive eigenvalues are positive and that eigenvalues have come in plus-minus pairs:
@@ -65,7 +66,7 @@ def bcs_soln(t, delta):
     # which I think is OK; for now, we'll just warn instead of assert
 #    assert numpy.min(evalsPos) > 1e-12
     if not numpy.min(evalsPos) > 1e-12:
-        logger.warning('some of evalsPos are not greater than 1e-12')
+        logger.warning('Some of evalsPos are not greater than 1e-12')
     assert numpy.max(numpy.abs(evalsNeg[::-1] + evalsPos)) < 1e-12
 
     u = evecsPos[0:evecsPos.shape[0]:2, :]
@@ -85,8 +86,8 @@ def bcs_soln(t, delta):
 
 
 def bcs_stats(u, v):
-    assert_ismatrix(u)
-    assert_ismatrix(v)
+    assert is_square_matrix(u)
+    assert is_square_matrix(v)
     assert u.shape[0] == v.shape[0]
 
     Nsites = u.shape[0]
@@ -114,7 +115,7 @@ def bcs_stats_average(u, v):
     stats = bcs_stats(u, v)
 
     if numpy.max(numpy.std(stats['Tvec'], 0)) > 1e-10:
-        logger.warning('this BCS state is not translationally invariant!  standard deviation of Tvec = ...')
+        logger.warning('This BCS state is not translationally invariant!  Standard deviation of Tvec = ...')
         logger.warning(numpy.std(stats['Tvec'], 0))
 
     return {k: numpy.mean(v, 0) for k, v in stats.items()}
@@ -133,38 +134,36 @@ def calculate_Tz(mu0_try, t_offsite, delta):
 
 
 def calculate_pairing_matrix(u, v, norm):
-    assert_ismatrix(u)
-    assert_ismatrix(v)
+    assert is_square_matrix(u)
+    assert is_square_matrix(v)
     assert u.shape[0] == v.shape[0]
 
     # Warn if u is singular, but don't abort cuz sometimes this is OK..
     (signdet, logdet) = numpy.linalg.slogdet(u)
-    logger.info('|det(u)| = %g', numpy.abs(numpy.exp(logdet)))  # just log it for now..
-#    if signdet * numpy.exp(logdet) == 0:
-#        logger.warning('|det(u)| = %g', numpy.abs(numpy.exp(logdet)))
+    logger.info('|det(u)| = %g', numpy.abs(numpy.exp(logdet)))  # actually, just log it for now..
 
     phiMat = numpy.dot( numpy.linalg.inv(u.conjugate().transpose()), v.conjugate().transpose() )
 
-#    logger.debug('pairing matrix = ...')
+#    logger.debug('Pairing matrix = ...')
 #    logger.debug(phiMat)
-#    logger.debug("pairing matrix's main diagonal = ...")
+#    logger.debug("Pairing matrix's main diagonal = ...")
 #    logger.debug(phiMat.diagonal())
 
     # Check that phiMat is symmetric:
     if numpy.max(numpy.abs(phiMat - phiMat.transpose())) > 1e-8:
-        logger.warning('"symmetricness" of pairing matrix = %.9g', numpy.max(numpy.abs(phiMat - phiMat.transpose())))
+        raise RuntimeError('"Symmetricness" of pairing matrix = %.9g', numpy.max(numpy.abs(phiMat - phiMat.transpose())))
 
     # Check against TI code (must first run *_Fourier.py with same parameters):
 #    phiMat_TI = numpy.loadtxt('phiMat_TI.dat').view(complex)
 #    logger.debug('max|phiMat - phiMat_TI| = %.9g ', numpy.max(numpy.abs(phiMat - phiMat_TI)))
 
     phiNorm = norm_of_pairing_matrix(phiMat)
-    logger.info('norm of pairing matrix before normalization = %f', phiNorm)
+    logger.info('Norm of pairing matrix before normalization = %f', phiNorm)
 
     if norm is not None:
         phiMat = norm * (phiMat / phiNorm)
         phiNorm = norm_of_pairing_matrix(phiMat)
-        logger.info('norm of pairing matrix after normalization = %f', phiNorm)
+        logger.info('Norm of pairing matrix after normalization = %f', phiNorm)
 
     return phiMat
 
@@ -174,7 +173,7 @@ def norm_of_pairing_matrix(phiMat):
 
 
 def plot_pairing_matrix(phiMat, callshow=True):
-    assert_ismatrix(phiMat)
+    assert is_square_matrix(phiMat)
 
     from mpl_toolkits.mplot3d import Axes3D
     from matplotlib import cm
@@ -231,7 +230,7 @@ def create_delta_matrix(lattice, boundary_conditions, delta_value, pairing_func,
 
 
 def add_onsite_terms(offsite, onsite):
-    assert_ismatrix(offsite)
+    assert is_square_matrix(offsite)
 
     check_offsite_for_onsite(offsite)  # since we're singling out the on-site terms with this function
 
@@ -251,13 +250,6 @@ def add_onsite_terms(offsite, onsite):
     return total
 
 
-# FIXME: this is pretty general; could be placed in a more general location..
-def assert_ismatrix(guy):
-    assert isinstance(guy, numpy.ndarray)
-    assert guy.ndim == 2
-    assert guy.shape[0] == guy.shape[1]
-
-
 def check_t_matrix(t):
     assert numpy.max(numpy.abs(t - t.conjugate().transpose())) < 1e-12
 
@@ -268,7 +260,7 @@ def check_delta_matrix(delta, channel):
     elif channel == 'triplet':
         assert numpy.max(numpy.abs(delta + delta.transpose())) < 1e-12
     else:
-        logger.warning('unable to check delta for appropriate pairing channel (singlet or triplet).')
+        logger.warning('Unable to check delta for appropriate pairing channel (singlet or triplet).')
 
 
 def check_offsite_for_onsite(offsite):
@@ -296,7 +288,6 @@ def dx2minusy2_pairing_pattern(lattice, site1, site2):
     Rhat = R / numpy.linalg.norm(R)
 
     return Rhat[0]**2 - Rhat[1]**2
-#    return did_pairing_pattern(lattice, site1, site2).real
 
 
 # hf is for 'half-filling' since the function currently solves for the root of Tz: Tz=0 (fdagf=1)
@@ -320,7 +311,7 @@ def did_hf_bcs_theory(lattice, boundary_conditions, t1, delta1, mu0_start=0, mu0
         mu0_soln = optimize.fsolve(calculate_Tz, mu0_start, args=(t_offsite, delta_offsite), full_output=True, xtol=1e-06, epsfcn=0.1)
         logger.info('mu0_soln = %s', mu0_soln)
         mu0 = mu0_soln[0][0]
-    #        mu0 = optimize.newton(calculate_Tz, mu0_start, tol=1e-06)
+#        mu0 = optimize.newton(calculate_Tz, mu0_start, tol=1e-06)
 
     t = add_onsite_terms(t_offsite, mu0)
     check_t_matrix(t)  # this would only fail if mu0 were complex or something
@@ -358,7 +349,7 @@ def dx2minusy2_hf_bcs_theory(lattice, boundary_conditions, t1, delta1, mu0_start
         mu0_soln = optimize.fsolve(calculate_Tz, mu0_start, args=(t_offsite, delta_offsite), full_output=True, xtol=1e-06, epsfcn=0.1)
         logger.info('mu0_soln = %s', mu0_soln)
         mu0 = mu0_soln[0][0]
-    #        mu0 = optimize.newton(calculate_Tz, mu0_start, tol=1e-06)
+#        mu0 = optimize.newton(calculate_Tz, mu0_start, tol=1e-06)
 
     t = add_onsite_terms(t_offsite, mu0)
     check_t_matrix(t)  # this would only fail if mu0 were complex or something
@@ -375,20 +366,22 @@ def dx2minusy2_hf_bcs_theory(lattice, boundary_conditions, t1, delta1, mu0_start
     return {'pairing_matrix':phi, 'chemical_potential':mu0, 'bcs_stats':stats}
 
 
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s %(levelname)s: %(message)s',
                         )
 
-    lattice = HexagonalLattice([12, 12])
+    lattice = HexagonalLattice([14, 14])
     boundary_conditions = (periodic, antiperiodic)
 
-    alpha = pi/100
+#    alpha = pi/4
 
-    bcs_theory = dx2minusy2_hf_bcs_theory(lattice, boundary_conditions, t1=numpy.cos(alpha), delta1=numpy.sin(alpha), mu0_start=0)
+#    bcs_theory = dx2minusy2_hf_bcs_theory(lattice, boundary_conditions, t1=numpy.cos(alpha), delta1=numpy.sin(alpha), mu0_start=0)
 #    bcs_theory = dx2minusy2_hf_bcs_theory(lattice, boundary_conditions, t1=1, delta1=5, mu0_start=0, mu0=None)
 
 #    bcs_theory = did_hf_bcs_theory(lattice, boundary_conditions, t1=numpy.cos(alpha), delta1=numpy.sin(alpha), mu0=0.1)
 #    bcs_theory = did_hf_bcs_theory(lattice, boundary_conditions, t1=1, delta1=5)
+    bcs_theory = did_hf_bcs_theory(lattice, boundary_conditions, t1=0, delta1=0.99, mu0=0, delta2=-1.01)
 
     plot_pairing_matrix(bcs_theory['pairing_matrix'])
--- pyvmc/scripts/run_dwave_sl_tri.py
+++ pyvmc/scripts/run_dwave_sl_tri.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from __future__ import division
 
 from pyvmc.core import HexagonalLattice, Bands, periodic, antiperiodic
@@ -66,71 +64,75 @@ def parameter_scan(theory_func, states_iterable, datadir, prefix, lattice, bound
         datafile.write('\n')
         datafile.flush()
     else:
+        # FIXME: this should check that boundary conditions and lattice dimensions in datapath actually match current job
         datafile = open(datapath, 'a')
 
 
     for wf_params_override in states_iterable:
-        for k, v in wf_params_override.items():
-            wf_params[k] = v
-        wf_params['norm'] = len(lattice) ** 1.0  # FIXME..
-#        wf_params['norm'] = None
+        try:
+            for k, v in wf_params_override.items():
+                wf_params[k] = v
+            wf_params['norm'] = len(lattice)
 
-        logger.info('starting a new state! wf_params = %s', wf_params)
-        bcs_theory = theory_func(lattice, boundary_conditions, **wf_params)
+            logger.info('starting a new state! wf_params = %s', wf_params)
+            bcs_theory = theory_func(lattice, boundary_conditions, **wf_params)
 
-        phi = bcs_theory['pairing_matrix']
-        wf = ProjectedBCSWavefunction(**{
-            'lattice': lattice,
-            'phi': phi,
-            'N_up': len(lattice) // 2,
-        })
+            phi = bcs_theory['pairing_matrix']
+            wf = ProjectedBCSWavefunction(**{
+                'lattice': lattice,
+                'phi': phi,
+                'N_up': len(lattice) // 2,
+            })
 
-        hamiltonian = HeisenbergPlusRingExchangeHamiltonian((periodic, periodic), lattice)
-        plans = [BasicOperatorMeasurementPlan(wf, o, steps_per_measurement=100) for o in hamiltonian.get_basic_operators()]
-        results = do_calculate_plans(plans, equilibrium_sweeps=100000, bins=30, measurement_sweeps_per_bin=30000)
-        result_lengths = [len(result) for result in results.itervalues()]
-        assert len(set(result_lengths)) == 1
-        evaluators = []
-        for i in xrange(result_lengths[0]):
-            context = {p.operator: result[i] for p, result in results.iteritems()}
-            evaluators.append(hamiltonian.evaluate(context))
+            hamiltonian = HeisenbergPlusRingExchangeHamiltonian((periodic, periodic), lattice)
+            plans = [BasicOperatorMeasurementPlan(wf, o, steps_per_measurement=200) for o in hamiltonian.get_basic_operators()]
+            results = do_calculate_plans(plans, equilibrium_sweeps=100000, bins=30, measurement_sweeps_per_bin=100000)
+            result_lengths = [len(result) for result in results.itervalues()]
+            assert len(set(result_lengths)) == 1
+            evaluators = []
+            for i in xrange(result_lengths[0]):
+                context = {p.operator: result[i] for p, result in results.iteritems()}
+                evaluators.append(hamiltonian.evaluate(context))
 
-        # get energies *per site*
-        Hami_terms = OrderedDict([
-            ('HeisNN', average_and_stddevmean([evaluator(J1=1, J2=0, J3=0, K=0) / len(lattice) for evaluator in evaluators])),
-            ('HeisNNN', average_and_stddevmean([evaluator(J1=0, J2=1, J3=0, K=0) / len(lattice) for evaluator in evaluators])),
-            ('HeisNNNN', average_and_stddevmean([evaluator(J1=0, J2=0, J3=1, K=0) / len(lattice) for evaluator in evaluators])),
-            ('ring4site', average_and_stddevmean([evaluator(J1=0, J2=0, J3=0, K=1) / len(lattice) for evaluator in evaluators])),
-        ])
+            # get exchange expectation values *per site*
+            Hami_terms = OrderedDict([
+                ('HeisNN', average_and_stddevmean([evaluator(J1=1, J2=0, J3=0, K=0) / len(lattice) for evaluator in evaluators])),
+                ('HeisNNN', average_and_stddevmean([evaluator(J1=0, J2=1, J3=0, K=0) / len(lattice) for evaluator in evaluators])),
+                ('HeisNNNN', average_and_stddevmean([evaluator(J1=0, J2=0, J3=1, K=0) / len(lattice) for evaluator in evaluators])),
+                ('ring4site', average_and_stddevmean([evaluator(J1=0, J2=0, J3=0, K=1) / len(lattice) for evaluator in evaluators])),
+            ])
 
-        for k, v in Hami_terms.items():
-            logger.info('%s = %.6f (%.6f)', k, v[0], v[1])
+            for k, v in Hami_terms.items():
+                logger.info('%s = %.6f (%.6f)', k, v[0], v[1])
 
-        # FIXME: also record acceptance rate in data file
-        output = {
-            't1': wf_params['t1'],
-            'delta1': wf_params['delta1'],
-            't2': wf_params['t2'],
-            'delta2': wf_params['delta2'],
-            't3': wf_params['t3'],
-            'delta3': wf_params['delta3'],
-            'delta0': wf_params['delta0'],
-            'mu0': bcs_theory['chemical_potential'],
-            'fdagf': bcs_theory['bcs_stats']['fdagf'],
-            'Ttot': bcs_theory['bcs_stats']['Ttot'],
-            'HeisNN': Hami_terms['HeisNN'][0], 'HeisNN_error': Hami_terms['HeisNN'][1],
-            'HeisNNN': Hami_terms['HeisNNN'][0], 'HeisNNN_error': Hami_terms['HeisNNN'][1],
-            'HeisNNNN': Hami_terms['HeisNNNN'][0], 'HeisNNNN_error': Hami_terms['HeisNNNN'][1],
-            'ring4site': Hami_terms['ring4site'][0], 'ring4site_error': Hami_terms['ring4site'][1],
-        }
+            # FIXME: also record acceptance rate in data file
+            output = {
+                't1': wf_params['t1'],
+                'delta1': wf_params['delta1'],
+                't2': wf_params['t2'],
+                'delta2': wf_params['delta2'],
+                't3': wf_params['t3'],
+                'delta3': wf_params['delta3'],
+                'delta0': wf_params['delta0'],
+                'mu0': bcs_theory['chemical_potential'],
+                'fdagf': bcs_theory['bcs_stats']['fdagf'],
+                'Ttot': bcs_theory['bcs_stats']['Ttot'],
+                'HeisNN': Hami_terms['HeisNN'][0], 'HeisNN_error': Hami_terms['HeisNN'][1],
+                'HeisNNN': Hami_terms['HeisNNN'][0], 'HeisNNN_error': Hami_terms['HeisNNN'][1],
+                'HeisNNNN': Hami_terms['HeisNNNN'][0], 'HeisNNNN_error': Hami_terms['HeisNNNN'][1],
+                'ring4site': Hami_terms['ring4site'][0], 'ring4site_error': Hami_terms['ring4site'][1],
+            }
 
-        data2write = numpy.array( [ output[k] for k in info_keys ] )
-        data2write.tofile(datafile, '  ')
-        datafile.write('\n')
-        datafile.flush()
+            data2write = numpy.array( [ output[k] for k in info_keys ] )
+            data2write.tofile(datafile, '  ')
+            datafile.write('\n')
+            datafile.flush()
 
-        if use_prev_mu:
-            wf_params['mu0_start'] = bcs_theory['chemical_potential']
+            if use_prev_mu:
+                wf_params['mu0_start'] = bcs_theory['chemical_potential']
+
+        except Exception as e:
+            logger.error('Exception raised: %r %r', wf_params, e)
 
     datafile.close()
 
@@ -138,11 +140,10 @@ def parameter_scan(theory_func, states_iterable, datadir, prefix, lattice, bound
 if __name__ == "__main__":
     from pyvmc.library.mft_utils import did_hf_bcs_theory, dx2minusy2_hf_bcs_theory
 
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s %(levelname)s: %(message)s',
                         )
     logging.getLogger().handlers[0].setLevel(logging.ERROR)
-#    logging.StreamHandler().setLevel(logging.ERROR)
 
     def nn_alpha_parameters():
         for alpha in numpy.linspace(pi/100, pi/2, 50):
@@ -172,7 +173,7 @@ if __name__ == "__main__":
             }
 
     def nnnCenke_beta_parameters():
-        for beta in numpy.linspace(0, pi/2, 51):
+        for beta in numpy.linspace(-pi/2, pi/2, 101):
             logger.info('starting new state, beta = %f', beta)
             yield {
                 't1': 0,
@@ -185,32 +186,19 @@ if __name__ == "__main__":
 
     parameter_scan(
        theory_func = dx2minusy2_hf_bcs_theory,
-       states_iterable = nn_Samuel_parameters(),
-#       datadir = '/Users/mishmash/Documents/Research/UCSB/Cenke_SL/Data/nodald/2013-02-10',
-       datadir = '.',
-       prefix = 'nodald_nn_SamuelComp',
-       lattice = HexagonalLattice([18, 18]),
-       boundary_conditions = (periodic, antiperiodic),  # for partons in mft (physical bound. conds. will always be fully periodic)
+       states_iterable = nn_alpha_parameters(),
+       datadir = '/Users/mishmash/Documents/Research/UCSB/Cenke_SL/Data/nodald/2013-02-18',
+#       datadir = '.',
+       prefix = 'nodald_nn_test',
+       lattice = HexagonalLattice([10, 11]),
+       boundary_conditions = (antiperiodic, antiperiodic),  # for partons in mft (physical bound. conds. will always be fully periodic)
        wf_params = {
-       'delta0': 0,
-       'mu0_start': 0,  # FIXME: be careful with our root-finder at the moment; it's sensitive to mu0_start..
+#           'mu0': 0,
+           'delta0': 0,
+           'mu0_start': 0,  # FIXME: be careful with our root-finder at the moment; it's sensitive to mu0_start..
        },
-       use_prev_mu = False,
+       use_prev_mu = True,
     )
-
-#    parameter_scan(
-#        theory_func = did_hf_bcs_theory,
-#        states_iterable = nnnCenke_beta_parameters(),
-#        datadir = '/Users/mishmash/Documents/Research/UCSB/Cenke_SL/Data/did/2012-02-10',
-#        prefix = 'did_nnnCenke',
-#        lattice = HexagonalLattice([18, 18]),
-#        boundary_conditions = (periodic, antiperiodic),  # for partons in mft (physical bound. conds. will always be fully periodic)
-#        wf_params = {
-#            'delta0': 0,
-#            'mu0_start': 0.0,  # FIXME: be careful with our root-finder at the moment; it's sensitive to mu0_start..
-#        },
-#        use_prev_mu = False,
-#    )
 
 #    from pyvmc.utils.parameter_iteration import iterate_parameters
 #    parameter_scan([d for d in iterate_parameters(['t1', 'delta1']) if d['delta1'] != 0])

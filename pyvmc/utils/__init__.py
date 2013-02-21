f1444ef6dc56216b9d6a2fa69448a98266c12cae
Jim Garrison <garrison@physics.ucsb.edu>
2013-02-20 19:39:41 -0800

    removed add_hc() function, which is now unused

--- pyvmc/utils/__init__.py
+++ pyvmc/utils/__init__.py
@@ -7,10 +7,6 @@ import numpy
 def product(iterable):
     return reduce(operator.mul, iterable)
 
-def add_hc(x):
-    """Returns the value plus its complex conjugate"""
-    return 2 * x.real
-
 def ensure_real(x):
     """Returns a real number as a real data type
 

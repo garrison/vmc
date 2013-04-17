5fd4af3c7df9e9c866ffc17e495efac35520d81d
Jim Garrison <garrison@physics.ucsb.edu>
2013-04-17 00:14:47 -0700

    added run_pbs_thread() function to torque.py

--- pyvmc/utils/torque.py
+++ pyvmc/utils/torque.py
@@ -1,4 +1,9 @@
 import re
+import os
+import logging
+from itertools import islice
+
+logger = logging.getLogger(__name__)
 
 def make_flag_property(pattern, post=lambda x: x, default=None):
     r = re.compile(pattern)
@@ -35,6 +40,42 @@ class PBSFlags(object):
     nodes = make_flag_property(r'^-l\s+nodes=(\d+):ppn=\d+$', int, default=1)
     ppn = make_flag_property(r'^-l\s+nodes=\d+:ppn=(\d+)$', int, default=1)
     array_ids = make_flag_property(r'^-t\s+(.*)$', get_array_ids)
+
+def run_pbs_thread(do_work, iterable, src_filename):
+    if "PBS_JOBID" not in os.environ:
+        # not running under PBS, so we only have one thread
+        do_work(iterable)
+        return
+
+    # fixme: some day use python threading instead
+    # (http://docs.python.org/2/library/queue.html)
+
+    array_id = int(os.environ.get("PBS_ARRAYID", 0))
+
+    pbs_flags = PBSFlags(src_filename)
+    assert pbs_flags.nodes == 1
+    ppn = pbs_flags.ppn
+    array_ids = pbs_flags.array_ids
+
+    step = ppn * len(array_ids)
+
+    # fork into processes numbered in the range [0, ppn)
+    pid = 0
+    for core_id in range(ppn - 1):
+        pid = os.fork()
+        if pid:
+            break
+    else:
+        core_id = ppn - 1
+
+    offset = array_ids.index(array_id) * ppn + core_id
+    logger.info("offset %d of %d", offset, step)
+
+    try:
+        do_work(islice(iterable, offset, None, step))
+    finally:
+        if pid:
+            os.waitpid(pid, 0)
 
 if __name__ == "__main__":
     flags = PBSFlags('/home/garrison/vmc-studies/electron-ring/2leg/rho-2_3/dmetal-rho-two-thirds-scan.py')

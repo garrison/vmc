2ca2ba8cc5e14d7b86b881c2f8ca9a01e0232ec2
Jim Garrison <garrison@physics.ucsb.edu>
2013-05-17 22:37:46 -0700

    make VMC_AMPLITUDE_T configurable

--- vmc-core/vmc-typedefs.hpp
+++ vmc-core/vmc-typedefs.hpp
@@ -13,8 +13,13 @@ typedef std::complex<real_t> complex_t;
 
 typedef real_t probability_t;
 
+#ifdef VMC_AMPLITUDE_T
+typedef VMC_AMPLITUDE_T amplitude_t;
+#else
 typedef complex_t amplitude_t;
-typedef complex_t phase_t;
+#endif
+
+typedef amplitude_t phase_t;
 
 static const unsigned int MAX_DIMENSION = 3;
 static const unsigned int MAX_MOVE_SIZE = 4;

f37ee412982423724130adc0a3a3e972ba43a2e0
Jim Garrison <garrison@physics.ucsb.edu>
2013-05-17 23:18:51 -0700

    complex_pow() now works with more general types

--- vmc-core/vmc-math-utils.hpp
+++ vmc-core/vmc-math-utils.hpp
@@ -29,7 +29,7 @@ static inline int safe_modulus (int n, int d)
 /**
  * Same as std::pow(), but returns zero instead of domain error for 0 ^ z when z < 0
  */
-static inline real_t safe_pow (real_t base, real_t exponent)
+static inline real_t safe_pow (const real_t &base, const real_t &exponent)
 {
     if (base == 0.0)
         return 0.0;
@@ -39,18 +39,20 @@ static inline real_t safe_pow (real_t base, real_t exponent)
 /**
  * Returns base * abs(base) ^ (exponent - 1)
  */
-static inline complex_t complex_pow (complex_t base, real_t exponent)
+template <typename T>
+static inline T complex_pow (const T &base, const real_t &exponent)
 {
-    complex_t rv = base;
+    T rv(base);
     if (exponent != 1.0) {
         rv *= safe_pow(std::abs(base), exponent - 1.0);
     }
     return rv;
 }
 
-static inline Big<complex_t> complex_pow (Big<complex_t> base, real_t exponent)
+template <typename T>
+static inline Big<T> complex_pow (const Big<T> &base, const real_t &exponent)
 {
-    return Big<complex_t>(complex_pow(base.get_base(), exponent), base.get_exponent() * exponent);
+    return Big<T>(complex_pow(base.get_base(), exponent), base.get_exponent() * exponent);
 }
 
 #endif

3b6ead5968107f5f1c9f5c952da38e7ea4bbde11
Jim Garrison <garrison@physics.ucsb.edu>
2013-05-23 16:07:11 -0700

    removed VMC_REAL_T and VMC_AMPLITUDE_T macros, now that we can instead use templates

--- vmc-core/vmc-typedefs.hpp
+++ vmc-core/vmc-typedefs.hpp
@@ -3,22 +3,12 @@
 
 #include <complex>
 
-#ifdef VMC_REAL_T
-typedef VMC_REAL_T real_t;
-#else
 typedef double real_t;
-#endif
-
 typedef std::complex<real_t> complex_t;
 
 typedef real_t probability_t;
 
-#ifdef VMC_AMPLITUDE_T
-typedef VMC_AMPLITUDE_T amplitude_t;
-#else
 typedef complex_t amplitude_t;
-#endif
-
 typedef amplitude_t phase_t;
 
 static const unsigned int MAX_DIMENSION = 3;

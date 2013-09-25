08d26c5cd30e33ed83f2c3a529b169fe833d4af9
Jim Garrison <garrison@physics.ucsb.edu>
2013-09-25 08:29:13 -0700

    doc for some_random_configuration()

--- vmc-core/random-configuration.hpp
+++ vmc-core/random-configuration.hpp
@@ -10,6 +10,13 @@
 
 class RandomNumberGenerator;
 
+/**
+ * Constructs a "random" configuration of the lattice at a given filling.
+ *
+ * This function will at times perform some heuristics to try to get a nonzero
+ * wavefunction.  For example, one such heuristic is to place each particle on
+ * a different leg of a ladder until all are full.
+ */
 extern std::vector<unsigned int> some_random_configuration (unsigned int N_filled, const Lattice &lattice, RandomNumberGenerator &rng);
 
 #endif
